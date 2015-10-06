7569ef96bcfe4af9dc15fa2473a0771e03157030
Jun Burden <junburden@gmail.com>
2015-10-06 13:06:21 +1300

    Create terrain stub files and add to cmake file

---
+++ Project/work/src/terrain.cpp
@@ -0,0 +1,36 @@
+//---------------------------------------------------------------------------
+//
+// Copyright (c) 2015 Taehyun Rhee, Joshua Scott, Ben Allen
+//
+// This software is provided 'as-is' for assignment of COMP308 in ECS,
+// Victoria University of Wellington, without any express or implied warranty. 
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+//
+// The contents of this file may not be copied or duplicated in any form
+// without the prior permission of its owner.
+//
+//----------------------------------------------------------------------------
+
+#include <cmath>
+#include <iostream> // input/output streams
+#include <fstream>  // file streams
+#include <sstream>  // string streams
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+#include "comp308.hpp"
+#include "terrain.hpp"
+
+using namespace std;
+using namespace comp308;
+
+
+Terrain::Terrain(string filename) {
+	cout<<filename<<endl;
+}
+
+void Terrain::renderTerrain() {
+
+}
---
+++ Project/work/src/terrain.hpp
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+//
+// Copyright (c) 2015 Taehyun Rhee, Joshua Scott, Ben Allen
+//
+// This software is provided 'as-is' for assignment of COMP308 in ECS,
+// Victoria University of Wellington, without any express or implied warranty. 
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+//
+// The contents of this file may not be copied or duplicated in any form
+// without the prior permission of its owner.
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "comp308.hpp"
+
+class Terrain {
+private:
+
+
+public:
+	Terrain(std::string);
+
+	void renderTerrain();
+};
