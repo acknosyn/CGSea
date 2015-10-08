d1c1fae8f0a4236620f5f6ddfbad8389d10462d7
Francesco Badraun <fabadraun@gmail.com>
2015-10-08 15:56:45 +1300

    Create fish header and C++ file and modify CMakeLists.txt to include these files

---
+++ Project/work/src/fish.cpp
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------------
+//
+// Francesco Badraun 2015
+//
+//----------------------------------------------------------------------------
+
+#include <cmath>
+#include <iostream> // input/output streams
+#include <fstream>  // file streams
+#include <sstream>  // string streams
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+#include "comp308.hpp"
+#include "fish.hpp"
+
+using namespace std;
+using namespace comp308;
+
+Fish::Fish() {
+	// init
+}
+
+void Fish::renderFish() {
+	// draw body with primitives
+}
---
+++ Project/work/src/fish.hpp
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+//
+// Francesco Badraun 2015
+//
+//----------------------------------------------------------------------------
+
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "comp308.hpp"
+
+class Fish {
+private:
+	// add private fields here
+
+public:
+	Fish();
+
+	void renderFish();
+};
